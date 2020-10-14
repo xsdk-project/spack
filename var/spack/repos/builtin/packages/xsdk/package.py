3b6d46b91cf87a045b48ad43307072196154957c
Satish Balay <balay@mcs.anl.gov>
2020-10-14 08:44:39 -0500

    strumpack: toggle cuda in sync with xsdk

--- var/spack/repos/builtin/packages/xsdk/package.py
+++ var/spack/repos/builtin/packages/xsdk/package.py
@@ -151,8 +151,9 @@ class Xsdk(BundlePackage):
     depends_on('omega-h@9.29.0', when='@0.5.0 +omega-h')
     depends_on('omega-h@9.19.1', when='@0.4.0 +omega-h')
 
+    depends_on('strumpack ~cuda', when='~cuda @0.6.0:')
     depends_on('strumpack@master', when='@develop +strumpack')
-    depends_on('strumpack@5.0.0~cuda', when='@0.6.0 +strumpack')
+    depends_on('strumpack@5.0.0', when='@0.6.0 +strumpack')
     depends_on('strumpack@3.3.0', when='@0.5.0 +strumpack')
     depends_on('strumpack@3.1.1', when='@0.4.0 +strumpack')
 
