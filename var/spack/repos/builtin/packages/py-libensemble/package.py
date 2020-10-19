7b523df1809acc90805ef4cf01f6e772eaddd83c
Satish Balay <balay@mcs.anl.gov>
2020-10-19 11:04:56 -0500

    revert py-libensemble changes [in favor of updates in develop]

--- var/spack/repos/builtin/packages/py-libensemble/package.py
+++ var/spack/repos/builtin/packages/py-libensemble/package.py
@@ -15,7 +15,6 @@ class PyLibensemble(PythonPackage):
     git      = "https://github.com/Libensemble/libensemble.git"
 
     version('develop', branch='develop')
-    version('0.7.1-xsdk', branch='develop')
     version('0.7.0', sha256='4c3c16ef3d4750b7a54198fae5d7ae402c5f5411ae85189da41afd20e20027dc')
     version('0.6.0', sha256='3f6a926d3868da53835ed93fc2e2a047b368dacb648c7608ee3a66debcee4d38')
     version('0.5.2', sha256='3e36c29a4a2adc0984ecfcc998cb5bb8a2cdfbe7a1ae92f7b35b06e41d21b889')
@@ -35,7 +34,6 @@ class PyLibensemble(PythonPackage):
     # depends_on('python@2.7:2.8,3.3:', when='@:0.4.1')
     # depends_on('python@3.5:', when='@0.5.0:')
     depends_on('python@3.5:')
-    depends_on('py-psutil', type=('build', 'run'), when='@0.7.1:')
     depends_on('py-setuptools', type='build')
     depends_on('py-numpy', type=('build', 'run'))
     depends_on('mpi', when='@:0.4.1')
