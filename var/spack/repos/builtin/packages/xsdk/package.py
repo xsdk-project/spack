0fba75c5c3c8fc31bd9f6a7ab79b9bac9a6ce695
Satish Balay <balay@mcs.anl.gov>
2020-11-02 12:55:00 -0600

    pumi update to version 2.2.5

--- var/spack/repos/builtin/packages/xsdk/package.py
+++ var/spack/repos/builtin/packages/xsdk/package.py
@@ -160,7 +160,7 @@ class Xsdk(BundlePackage):
     depends_on('strumpack@3.1.1', when='@0.4.0 +strumpack')
 
     depends_on('pumi@develop', when='@develop')
-    depends_on('pumi@2.2.4', when='@0.6.0')
+    depends_on('pumi@2.2.5', when='@0.6.0')
     depends_on('pumi@2.2.1', when='@0.5.0')
     depends_on('pumi@2.2.0', when='@0.4.0')
 
